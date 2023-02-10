fc3a1d64b30f979a1b6484b58df4cb9d41224267
kallyas <kallyasmedia@gmail.com>
2023-02-10 11:04:27 +0300

    fix: undefined vehichle entity adapter

--- client/src/features/vehichle/vehicleApiSlice.js
+++ client/src/features/vehichle/vehicleApiSlice.js
@@ -1,6 +1,10 @@
 import { createEntityAdapter, createSelector } from "@reduxjs/toolkit";
 import { apiSlice } from "../../api/apiSlice";
 
+const vehicleAdapter = createEntityAdapter();
+
+const initialState = vehicleAdapter.getInitialState({});
+
 export const vehicleApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     addVehicle: builder.mutation({
