83d83b3eff555b78b00df63f1835875a7ab91b0f
kallyas <kallyasmedia@gmail.com>
2023-02-10 12:02:06 +0300

    fix: car number plate and phone regex

--- client/src/util/validations.js
+++ client/src/util/validations.js
@@ -6,27 +6,38 @@ const loginSchema = yup.object().shape({
 });
 
 const vehichleSchema = yup.object().shape({
-  number_plate: yup.string().required("Number plate is required"),
+  number_plate: yup
+    .string()
+    .required("Number plate is required")
+    .matches(
+      /^U[A-Z]{2} [0-9]{3}[A-Z]$/,
+      "Invalid car number plate format. Example: UAB 456T"
+    ),
   driver: yup.string().required("Driver is required"),
   mileage: yup.string().required("Mileage is required"),
   manufacturer: yup.string().required("Manufacturer is required"),
   date_of_purchase: yup.string().required("Date of purchase is required"),
-})
-
+});
 
 const driverSchema = yup.object().shape({
   first_name: yup.string().required("First name is required"),
   last_name: yup.string().required("Last name is required"),
-  phone_number: yup.string().required("Phone number is required"),
+  phone_number: yup
+    .string()
+    .required("Phone number is required")
+    .matches(
+      /^0[7-9][0-9]{8}$/,
+      "Invalid phone number format. Example: 0752541359"
+    ),
   date_hired: yup.string().required("Date Hired is required"),
   age: yup.string().required("Age is required"),
-})
+});
 
 const mentainanceSchema = yup.object().shape({
   date: yup.string().required("Date is required"),
   description: yup.string().required("Description is required"),
   cost: yup.string().required("Cost is required"),
   fleet: yup.string().required("Vehicle is required"),
-})
+});
 
 export { loginSchema, driverSchema, vehichleSchema, mentainanceSchema };
